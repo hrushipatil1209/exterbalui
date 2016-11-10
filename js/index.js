30e5db8bdd26833428bc6a7eff5fb13444cf2a82
hrushipatil1209 <hrushikesh.patil@gmail.com>
2016-11-11 02:12:45 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+angular.module('myApp',['ngMaterial']);
+
+angular.module('myApp').controller('CallBackURL', function() {
+    var callCackURL = this;
+    callCackURL.exit = function () {
+        window.location="http://www.google.com"
+    }
+});
