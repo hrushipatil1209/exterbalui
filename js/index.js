e2618cf07cf2a7d566ae1ab96ff2dd5ee5ab8f85
hrushipatil1209 <hrushikesh.patil@gmail.com>
2016-11-11 12:19:56 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ angular.module('myApp').controller('CallBackURL', function() {
         window.location="https://hrushipatil1209.github.io/exterbalui/return.html"
     }
 });
+angular.module('myApp').controller('CallBackJS', function() {
+    var callCackJS = this;
+    callCackJS.exit = function () {
+       window.parent.parent_exit();
+    }
+});
